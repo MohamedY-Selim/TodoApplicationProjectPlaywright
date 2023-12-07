36848da5a831dd9e35fc8d12fa79d7188b1e0a40
Mohamed Yehia <mohamedy.selim14@gmail.com>
2023-12-07 12:05:17 +0200

    Add Headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     ignoreHTTPSErrors: true,
-    headless: false,
     baseURL: "https://todo.qacart.com",
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
